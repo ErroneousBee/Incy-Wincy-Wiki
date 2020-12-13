674e838dec64fe5fd4e66bcf9985fe7b2bd712d7
Neil Hancock <neil@neilhancock.co.uk>
2020-12-13 20:40:23 +0000

    Start moving to have nav side as markdown

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ function onload() {
     console.trace();
     read_file_into("content/logo.html", document.getElementById("logo"));
     read_file_into("content/mast.txt", document.getElementById("mast"));
-    read_file_into("content/sidenav.txt", document.getElementById("sidenav"));
+    read_file_into("content/sidenav.md", document.getElementById("sidenav"));
     read_file_into("content/test.txt", document.getElementById("content"));
 }
 
