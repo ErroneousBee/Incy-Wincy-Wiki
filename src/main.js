a7d5be449ec7adacd829e15926e4370771f73034
Neil Hancock <neil@neilhancock.co.uk>
2021-02-14 20:45:10 +0000

    Add theming via the config file

--- src/main.js
+++ src/main.js
@@ -16,6 +16,8 @@ const App = {
         // First, we wait for the config, how else will we know what to do?
         await App.load_config();
 
+        App.load_theme();
+
         // Load the assorted navigation, banners and whatnot.
         App.load_side_content();
 
@@ -296,7 +298,7 @@ const App = {
                 // Toggle clicked, turn off all siblings
                 const isopen = li.lastElementChild.classList.contains("open");
                 li.closest("ul").querySelectorAll("li>ul.open").forEach(sibling => sibling.classList.remove("open"));
-               
+
                 if (!isopen) {
                     li.lastElementChild.classList.add("open");
                 }
@@ -351,6 +353,15 @@ const App = {
 
         return App.get_path_from_element(elp, pathpart);
 
+    },
+
+    /**
+     * Set the theme href.
+     * @param {String} name 
+     */
+    load_theme() {
+        document.querySelector("link#theme_colors").setAttribute("href", "themes/" + Config.theme + "/colors.css");
+        document.querySelector("link#theme_layout").setAttribute("href", "themes/" + Config.theme + "/layout.css");
     }
 
 };
