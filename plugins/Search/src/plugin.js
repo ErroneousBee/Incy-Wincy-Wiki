3ed2dd916fad3cc4cef0618ee3aa921228a5b59a
Neil Hancock <neil@neilhancock.co.uk>
2021-08-08 13:28:09 +0100

    Finished search conversion to plugin Allow search args as a path in search

--- plugins/Search/src/plugin.js
+++ plugins/Search/src/plugin.js
@@ -5,12 +5,20 @@ App.Plugins.Search = {
     previews: LUNR_PREVIEW_LOOKUP,
 
     keyup(e) {
+        App.Plugins.Search.populate_results(e.target.value);
 
-        const value = e.target.value;
-        const results = App.Plugins.Search.index.search(value);
+    },
 
-        if (value === "" || results.length === 0) {
+    populate_results(value) {
+
+        if (value === "") {
             document.querySelector("div.searchresults").innerHTML = '<hr/>';
+            return;
+        }
+
+        const results = App.Plugins.Search.index.search(value);
+        if (results.length === 0) {
+            document.querySelector("div.searchresults").innerHTML = '<hr/><p>No results found.</p>';
             return;
         }
 
@@ -46,14 +54,29 @@ App.Plugins.Search = {
         }
 
         // Tell the page loader to call our onactivate when it sees a path called "Search"
-        App.register_divert( Config.search.path , App.Plugins.Search.onactivate )
+        App.register_divert(Config.search.path, App.Plugins.Search.onactivate)
 
     },
 
     async onactivate(path, config, element) {
-        console.log("Search Activated", path, config, element)
+        console.log("Search Activated", path, config, element);
+
+        // Copy the search input template into place
+        const template = document.getElementById("lunr_search_page_template").content.cloneNode(true);
+        element.innerHTML = "";
+        element.appendChild(template);
+
+        // We treat second path level as search args
+        const search_args = path.split("/", 2);
+        if (search_args.length > 1) {
+            const args =   decodeURIComponent(search_args[1]);
+            document.querySelector('div.searchform input#search').value = args;
+            App.Plugins.Search.populate_results(args);
+        }
+
+
     },
 
-    
+
 
 }
